1a84ec5241933527ae4c7c6a1e21afcf112cd9b0
Joohoon Kim <jayjhkim7@gmail.com>
2021-11-22 22:14:25 -0500

    wip browser actions

--- tests/trial.test.js
+++ tests/trial.test.js
@@ -2,10 +2,18 @@ const puppeteer = require('puppeteer');
 
 describe('first pptr trial', () => {
   it('should launch browser', async () => {
-    const browser = await puppeteer.launch({ headless: true, slowMo: 500 });
+    const browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 500,
+      devtools: false,
+    });
     const page = await browser.newPage();
     await page.goto('https://www.hiddenlevers.com');
+    await page.waitForTimeout(3000);
     await page.waitForSelector('h1');
+    await page.waitForTimeout(3000);
+    await page.reload();
+    await page.waitForSelector('h2');
     await browser.close();
   });
 });
