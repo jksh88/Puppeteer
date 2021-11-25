4f87d4990039feca2a51142c1d8fe226e6ef04d5
Joohoon Kim <jayjhkim7@gmail.com>
2021-11-25 14:46:17 -0500

    type, click, and some slectors

--- tests/trial.test.js
+++ tests/trial.test.js
@@ -12,7 +12,12 @@ describe('first pptr trial', () => {
     await page.type('#developer-name', 'Mike', { delay: 2 });
     await page.click('#tried-test-cafe', { clickCount: 1 });
     await page.select('#preferred-interface', 'Command Line');
+    const msg = 'Testing my input message';
+    await page.type('#comments', msg);
+    await page.click('#submit-button');
+    await page.waitForSelector('#article-header');
     await page.waitForTimeout(12000);
+
     await browser.close();
   });
 });
