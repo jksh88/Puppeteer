57843608b1b88b353c18f18320526021d96db653
Joohoon Kim <jayjhkim7@gmail.com>
2021-12-04 20:42:47 -0500

    wip feedback submission test not working

---
+++ tests/e2e/feedback.test.js
@@ -0,0 +1,38 @@
+const puppeteer = require('puppeteer');
+const expect = require('chai').expect;
+
+describe('Feedback test', () => {
+  let browser;
+  let page;
+
+  before(async () => {
+    browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 40,
+      devtools: false,
+    });
+    page = await browser.newPage();
+  });
+
+  after(async () => {
+    await browser.close();
+  });
+
+  it('Uploads feedback ok', async () => {
+    await page.goto('http://zero.webappsecurity.com/index.html');
+    await page.waitForSelector('#feedback');
+    await page.click('#feedback');
+    await page.waitForSelector('#name');
+    await page.type('#name', 'margarito');
+    await page.type('#email', 'email');
+    await page.type('#subject', 'marg');
+    await page.type('#comment', 'arg');
+    await page.click('input[name="submit"]');
+  });
+
+  it('Provides thank you note', async () => {
+    await page.waitForSelector('.page-header');
+    const text = await page.$eval('.page-header::after', (e) => e.textContent);
+    expect(text).to.contain('Thank you');
+  });
+});
